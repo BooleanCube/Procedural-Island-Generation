93c2988294fadcf780d103e6c6e3df8aac6a0fd4
BooleanCube <47650058+BooleanCube@users.noreply.github.com>
2022-02-14 17:28:48 -0500

    no set radius

---
+++ outwardexpansiondisperse.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+//there are no rivers in this proc gen method.
+
+public class outwardexpansiondisperse {
+    //statistics
+    private static int iterationCounter = 0;
+    private static long startTime = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        int c = n/2;
+        double r = n/2.0;
+        startTime = System.currentTimeMillis();
+        Node[][] map = new Node[n][n];
+        double offC = 2.4/n;
+        for(int i=0; i<r; i++) {
+            for(int j=0; j<r; j++) {
+                ++iterationCounter;
+                double d = Math.sqrt(Math.pow(i,2) + Math.pow(j,2));
+                //if(d>=r) continue;
+                double off = d*offC;
+                if(Math.random()>off) map[c+i][c+j] = new Node('*');
+                if(Math.random()>off) map[c-i][c+j] = new Node('*');
+                if(Math.random()>off) map[c+i][c-j] = new Node('*');
+                if(Math.random()>off) map[c-i][c-j] = new Node('*');
+            }
+        }
+        printMap(map);
+    }
+
+    //O(n)
+    static void printMap(Node[][] map) {
+        double time = (System.currentTimeMillis()-startTime)/1000.0;
+        StringBuilder sb = new StringBuilder();
+            for(Node[] a : map) {
+                for(Node b : a) {
+                    if(b != null) sb.append('*').append(" ");
+                    else sb.append("  ");
+                }
+                sb.append("\n");
+            }
+        System.out.println(sb.toString());
+        System.out.println("Iteration Count: " + iterationCounter + " iterations");
+        System.out.println("Runtime: " + time + " seconds");
+    }
+}
+
+class Node {
+    public char value;
+    public Node(char v) { value = v; }
+}
