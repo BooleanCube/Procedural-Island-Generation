2781c3ee64a41596e578e4b914750f6afb18c44c
BooleanCube <47650058+BooleanCube@users.noreply.github.com>
2022-03-13 01:45:09 -0500

    flood fill

--- floodfill.java
+++ floodfill.java
@@ -3,12 +3,18 @@ import java.util.*;
 
 public class floodfill {
     
+    //statistics
+    static int iterationCounter = 0;
+    static long startTime = 0;
+
+    //map
     static Node[][] map;
     
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bf.readLine());
         map = new Node[n][n];
+        startTime = System.currentTimeMillis();
         int c = n/2;
         map[c][c] = new Node('z',c,c);
         fillIsland(c, c, map[c][c], 2.4/n, n*0.4);
@@ -20,24 +26,27 @@ public class floodfill {
         else return;
         double d = Math.sqrt(Math.pow(x-center.x, 2) + Math.pow(y-center.y, 2));
         if(d>radius) return;
+        iterationCounter++;
         double off = d*offC;
         if(Math.random() > off) map[x][y].value = '*';
-        fillIsland(x+1, y+1, center, offC, radius);
-        fillIsland(x+1, y-1, center, offC, radius);
-        fillIsland(x-1, y+1, center, offC, radius);
-        fillIsland(x-1, y-1, center, offC, radius);
+        fillIsland(x+1, y, center, offC, radius);
+        fillIsland(x-1, y, center, offC, radius);
+        fillIsland(x, y+1, center, offC, radius);
+        fillIsland(x, y-1, center, offC, radius);
     }
 
     static void printMap() {
+        double time = (System.currentTimeMillis()-startTime)/1000d;
         StringBuilder sb = new StringBuilder();
         for(Node[] l : map) {
             for(Node n : l) {
                 if(n != null) sb.append(n.value).append(' ');
-                else sb.append(" ");
+                else sb.append("  ");
             }
             sb.append('\n');
         }
         System.out.println(sb.toString());
+        System.out.println("Iteration Counter: " + iterationCounter + "\nRuntime: " + time + " seconds");
     }
 
 }
